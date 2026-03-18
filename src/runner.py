2b2de059dc0a89c2c5929d392715e867f6769c70
aborecevskij94 <aborecevskij94@gmail.com>
2026-03-18 16:59:06 +0000

    fix: import errors

--- src/runner.py
+++ src/runner.py
@@ -3,7 +3,9 @@ This is the main runner file that will be executed by the GitHub action.
 """
 
 from config import GITHUB_REPOSITORY_OWNER, GITHUB_REPOSITORY_NAME
-from services import prompt, ai, git
+from services.ai.service import AiRequest
+from services.git.service import GitHub
+from services.prompt.service import PromptGenerator
 
 
 def run(
@@ -16,19 +18,19 @@ def run(
     :param repository:
     :return:
     """
-    git_client = git.service.GitHub(
+    git_client = GitHub(
         owner=owner,
         repo=repository
     )
     files = git_client.get_pr_files_content()
 
-    prompt_client = prompt.service.PromptGenerator(
+    prompt_client = PromptGenerator(
         system_prompt="System prompt",
         context_prompt=files
     )
     context = prompt_client.get_prompt()
 
-    ai_client = ai.service.AiRequest(
+    ai_client = AiRequest(
         context=context
     )
     response = ai_client.get_response()
