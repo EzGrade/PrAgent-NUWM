c11b2a2efe7e96c079f5960898e45961060803bd
aborecevskij94 <aborecevskij94@gmail.com>
2026-03-19 14:53:33 +0000

    ref

--- src/config.py
+++ src/config.py
@@ -1,7 +1,6 @@
 """
 This module contains configuration variables for the application.
 """
-from google.auth import default
 
 from utils.environment import get_env_var
 
@@ -10,7 +9,6 @@ GITHUB_APP_ID = int(get_env_var("APP_ID"))
 if GITHUB_PRIVATE_KEY.endswith(".pem"):
     with open(GITHUB_PRIVATE_KEY, "r", encoding="utf-8") as key_file:
         GITHUB_PRIVATE_KEY = key_file.read()
-        print(GITHUB_PRIVATE_KEY)
 GITHUB_INSTALLATION_ID = get_env_var("INSTALLATION_ID", default=None)
 
 GITHUB_REPOSITORY = get_env_var("GITHUB_REPOSITORY").split("/")
@@ -21,7 +19,7 @@ OPENAI_API_KEY = get_env_var("OPENAI_API_KEY")
 OPENAI_MODEL = get_env_var("OPENAI_MODEL")
 
 CREDENTIALS_CONTENT = get_env_var("GOOGLE_CREDENTIALS_CONTENT", default='')
-SPREADSHEET_ULR = get_env_var("SPREADSHEET", default='')
+SPREADSHEET_URL = get_env_var("SPREADSHEET", default='')
 
 DEFAULT_PROMPT = """You are a teacher reviewing a student's code.
 You should give only advices, not complete code.
@@ -32,3 +30,5 @@ It will be posted on github, so use markups inside body
 The student has submitted the following files for review:"""
 
 PROMPT = get_env_var("PROMPT", default=DEFAULT_PROMPT)
+
+LOGGING_LEVEL = get_env_var("LOGGING_LEVEL", default="INFO")
