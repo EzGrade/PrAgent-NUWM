26128cecb81e3e23014b2aa72eaedfe7c2cac7f4
aborecevskij94 <aborecevskij94@gmail.com>
2026-03-25 15:18:16 +0000

    fix: cfg

--- src/configs/github.py
+++ src/configs/github.py
@@ -5,14 +5,10 @@ from .base import BaseApplicationConfig
 
 
 class GitHubConfig(BaseApplicationConfig):
-    APP_ID: int = Field(..., description="GitHub App ID")
-    INSTALLATION_ID: int | None = Field(default=None, description="GitHub Installation ID")
-    REPOSITORY: str = Field(..., description="GitHub Repository")
-    PRIVATE_KEY: str = Field(..., description="GitHub Private Key")
-
-    model_config = SettingsConfigDict(
-        env_prefix="GIT_"
-    )
+    GIT_APP_ID: int = Field(..., description="GitHub App ID")
+    GIT_INSTALLATION_ID: int | None = Field(default=None, description="GitHub Installation ID")
+    GITHUB_REPOSITORY: str = Field(..., description="GitHub Repository", alias="GITHUB_REPOSITORY")
+    GIT_PRIVATE_KEY: str = Field(..., description="GitHub Private Key")
 
     @model_validator(mode="before")
     def validate_installation_id(cls, values):
